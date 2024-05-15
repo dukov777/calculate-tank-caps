a3a09b3c8a79f76b549a0909d7eb082e3188f8d9
Petar Lalov <petar.lalov@gmail.com>
2024-05-15 08:03:36 +0300

    Fixed review comments.

--- main.cpp
+++ main.cpp
@@ -2,12 +2,24 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "main.h"
 
+struct ProgramData
+{
+    float i;
+    float f;
+    std::vector<std::string> group1;
+    std::vector<std::string> group2;
+};
 
-int main(int argc, char** argv) {
-    // Create an ArgumentParser object
+ProgramData get_commnad_line_params(int argc, char **argv)
+{
+    ProgramData data;
+
+    // create a new ArgumentParser object
     argparse::ArgumentParser program("calculate-tank-caps");
 
+    // add program arguments
     program.add_argument("-i")
         .help("Current")
         .default_value(0.0f)
@@ -21,46 +33,71 @@ int main(int argc, char** argv) {
     program.add_argument("-group1")
         .help("Capacitors in group 1. Minimum 1 capacitors required. Maximum 5 capacitors allowed.")
         .nargs(1, 5)
-        .action([](const std::string& value) { return value; });
+        .action([](const std::string &value)
+                { return value; });
 
     program.add_argument("-group2")
         .help("Capacitors in group 2. Minimum 1 capacitors required. Maximum 5 capacitors allowed.")
         .nargs(1, 5)
-        .action([](const std::string& value) { return value; });
+        .action([](const std::string &value)
+                { return value; });
 
-    try {
+    try
+    {
         // Parse the command line arguments
         program.parse_args(argc, argv);
     }
-    catch (const std::runtime_error& err) {
+    catch (const std::runtime_error &err)
+    {
+        // dump help message and exit
         std::cerr << err.what() << std::endl;
         std::cerr << program;
         exit(EXIT_FAILURE);
     }
 
-    auto i = program.get<float>("-i");
-    std::cout << "i: " << i << std::endl;
+    // Get the CLI values
+    data.i = program.get<float>("-i");
+    data.f = program.get<float>("-f");
+    data.group1 = program.get<std::vector<std::string>>("-group1");
+    data.group2 = program.get<std::vector<std::string>>("-group2");
 
-    auto f = program.get<float>("-f");
-    std::cout << "f: " << f << std::endl;
-        
-    auto group1 = program.get<std::vector<std::string>>("-group1"); // {1.95, 2.47}
-    if (!group1.empty()) {
-        std::cout << "group1: ";
-        for (auto &v : group1) {
-            std::cout << v << " ";
-        }
-        std::cout << std::endl;
+    return data;
+}
+
+int main(int argc, char **argv)
+{
+    // get the command line parameters
+    ProgramData data = get_commnad_line_params(argc, argv);
+
+    // validate constraints on the input data
+    if (data.group1.size() < 1 || data.group1.size() > 5)
+    {
+        std::cerr << "Error: Capacitors in group 1. Minimum 1 capacitors required. Maximum 5 capacitors allowed." << std::endl;
+        exit(EXIT_FAILURE);
     }
 
-    auto group2 = program.get<std::vector<std::string>>("-group2");
-    if (!group2.empty()) {
-        std::cout << "group2: ";
-        for (auto &v : group2) {
-            std::cout << v << " ";
-        }
-        std::cout << std::endl;
+    if (data.group2.size() < 1 || data.group2.size() > 5)
+    {
+        std::cerr << "Error: Capacitors in group 2. Minimum 1 capacitors required. Maximum 5 capacitors allowed." << std::endl;
+        exit(EXIT_FAILURE);
     }
+
+    // Output the data just for clarity
+    std::cout << "i: " << data.i << std::endl;
+    std::cout << "f: " << data.f << std::endl;
+    std::cout << "group1: ";
+    for (auto &v : data.group1)
+    {
+        std::cout << v << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "group2: ";
+    for (auto &v : data.group2)
+    {
+        std::cout << v << " ";
+    }
+    std::cout << std::endl;
 
     return 0;
 }
